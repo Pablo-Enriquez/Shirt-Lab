a2ea93df9c4a2d38cc8f4e4fc5d76b164c473b21
Pablo-Enriquez <36930307+Pablo-Enriquez@users.noreply.github.com>
2018-03-08 11:39:50 -0600

    Add files via upload

---
+++ MainCloset.py
@@ -0,0 +1,12 @@
+from Graphics import *
+
+class shirt(object):
+    """This creates a basic colored shirt"""
+    def __init__(self, x, y, color, win):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def drawShirt(self, x, y, color, win):
+        shirt
+
